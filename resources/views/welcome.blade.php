d09e85944edb1a0c0abf2daa202b1595fd91e372
Joshua Foxworth <joshuafoxworth@Admins-Air.hsd1.tx.comcast.net>
2017-06-29 15:22:18 -0500

    test new template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,7 +108,7 @@
 
         <!-- Intro section end -->
 
-        <!-- Profile contact callout section -->
+        <!-- Profile contact callout section --
 
         <section id="profile-contact">
 
@@ -137,9 +137,9 @@
                         </div>
                     </div>
 
-                </div><!-- .row -->
+                </div>
 
-            </div><!-- .container -->
+            </div>
 
         </section>
 
@@ -159,7 +159,7 @@
                     </div>
 
                     <div class="col-md-3 col-sm-6 hidden-xs wow bounceInLeft">
-                        <img class="avatar" src="/images/drawingroom.jpg" alt="">
+                        <img class="avatar" src="/images/capsule_head.jpeg" alt="">
                     </div>
 
                     <div class="col-md-3 col-sm-6 wow bounceInUp">
@@ -167,7 +167,7 @@
                         <div class="profile-widget">
                             <h3>Skillset</h3>
 
-                            <h5>UX Design</h5>
+                            <h5>Structural Engineering</h5>
                             <div class="skill-bar">
                                 <div class="skill-rate-on"></div>
                                 <div class="skill-rate-on"></div>
@@ -175,7 +175,15 @@
                                 <div class="skill-rate-off"></div>
                             </div>
 
-                            <h5>HTML / CSS3 / SASS</h5>
+                            <h5>Front End Web Development</h5>
+                            <div class="skill-bar">
+                                <div class="skill-rate-on"></div>
+                                <div class="skill-rate-on"></div>
+                                <div class="skill-rate-on"></div>
+                                <div class="skill-rate-off"></div>
+                            </div>
+
+                            <h5>Back End Web Development</h5>
                             <div class="skill-bar">
                                 <div class="skill-rate-on"></div>
                                 <div class="skill-rate-on"></div>
@@ -183,18 +191,10 @@
                                 <div class="skill-rate-off"></div>
                             </div>
 
-                            <h5>Javascript</h5>
+                            <h5>Deployment / AWS</h5>
                             <div class="skill-bar">
                                 <div class="skill-rate-on"></div>
                                 <div class="skill-rate-on"></div>
-                                <div class="skill-rate-on"></div>
-                                <div class="skill-rate-on"></div>
-                            </div>
-
-                            <h5>Ajax</h5>
-                            <div class="skill-bar">
-                                <div class="skill-rate-on"></div>
-                                <div class="skill-rate-off"></div>
                                 <div class="skill-rate-off"></div>
                                 <div class="skill-rate-off"></div>
                             </div>
@@ -214,8 +214,9 @@
 
                     <div class="col-md-6 col-sm-12 wow bounceInRight">
                         <h3>Profesional Profile</h3>
-                        <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend. I am so happy, my dear friend.</p>
-                        <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend.</p>
+                        <p>My schooling and degrees are in aerospace engineering with an emphasis on structures and fluid flow. After school, I worked in the space program as an engineer and designed components for the Orion Capsule. After leaving the space program, I began to develop startup websites on both the front and back end. This has led to me become proficient in a number of areas without really specializing in any one area.</p>
+                        <p>In college and in the space program, I began to understand the level of inequality in education and employment based upon race and gender and I started to advocate for equality for my sons.
+                        <p>This site provides my resume, describes the work that I have done, and lists my skill set. If you want to contact me, that info is also provided.</p>
                     </div>
 
                 </div><!-- .row -->
