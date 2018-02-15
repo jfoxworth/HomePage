b67e0eb25cfc438acb1efea6b56998c15ca0ab81
Joshua Foxworth <jfoxworth@cadwolf.com>
2018-02-14 21:48:16 -0600

    formatting

--- resources/views/candidate.blade.php
+++ resources/views/candidate.blade.php
@@ -2,69 +2,73 @@
 
     <section id="aboutme" class="section">
 
-        <div class="container">
+    <div class="container">
 
-            <div class="row">
+        <div class="row">
 
-                <div class="col-md-12 headline wow bounceInDown">
-                    <h2>Joshua Foxworth</h2>
-                    <p>Candidate for Precinct Chair - 0487</p>
-                </div>
+            <div class="col-md-12 headline wow bounceInDown">
+                <h2>Joshua Foxworth</h2>
+                <p>Candidate for Precinct Chair - 0487</p>
+            </div>
 
-                <div class="col-md-7 col-sm-6 wow">
-                    <h3>A bit about me</h3>
-                    <p>I am an engineer, entrepreneur, and web developer that has lived here in Brittany Lakes since 2008. I am married with two young boys and I am a member of Bay Area Church. My wife and I are both aerospace engineers and graduates of UT Austin. Most evenings, you can find us out by Willow Pond with the kids.</p>
+            <div class="col-md-7 col-sm-6 wow">
+                <h3>A bit about me</h3>
+                <p>I am an engineer, entrepreneur, and web developer that has lived here in Brittany Lakes since 2008. I am married with two young boys and I am a member of Bay Area Church. My wife and I are both aerospace engineers and graduates of UT Austin. Most evenings, you can find us out by Willow Pond with the kids.</p>
 
-                    <ul>
-                        <li>USMC Veteran</li>
-                        <li>BS and MS from UT Austin in Aerospace Engineering</li>
-                        <li>Resident of Brittany Lakes since 2008</li>
-                        <li>Member of Bay Area Church since 2010.</li>
-                        <li>Entrepreneur</li>
-                    </ul>
-                </div>
+                <ul>
+                    <li>USMC Veteran</li>
+                    <li>BS and MS from UT Austin in Aerospace Engineering</li>
+                    <li>Resident of Brittany Lakes since 2008</li>
+                    <li>Member of Bay Area Church since 2010.</li>
+                    <li>Entrepreneur</li>
+                </ul>
+            </div>
 
-                <div class="col-md-5 col-sm-6 hidden-xs wow bounceInLeft">
-                    <img class="avatar" src="/images/drawingroom.jpg" alt="">
-                </div>
-
+            <div class="col-md-5 col-sm-6 hidden-xs wow bounceInLeft">
+                <img class="avatar" src="/images/drawingroom.jpg" alt="">
             </div>
 
         </div>
+
+    </div>
 
     </section>
 
 
     <section id="whattodo" class="section">
 
-        <div class="container">
+    <div class="container">
 
-            <div class="row">
+        <div class="row">
 
-                <div class="col-md-7 col-sm-12 wow">
-                    <h3>What I want to do as precinct chair</h3>
-                    
-                    <p>The GOP is facing a number of problems within the county, the congressional district, and the state. These problems include getting information to our constituents, getting feedback on local and national issues, and an opponent with a web presence that is much more organized. I want to change all of that, so here is what I would do as precinct chair ...</p>
+            <div class="col-md-7 col-sm-12 wow">
+                <h3>What I want to do as precinct chair</h3>
+                
+                <p>The GOP is facing a number of problems within the county, the congressional district, and the state. These problems include getting information to our constituents, getting feedback on local and national issues, and an opponent with a web presence that is much more organized. I want to change all of that, so here is what I would do as precinct chair ...</p>
 
+                <ul>
+                    <li>Start a facebook group for this precinct where residents could get updates on local elections, events, budget meetings, etc</li>
                     <ul>
-                        <li>Start a facebook group for this precinct where residents could get updates on local elections, events, budget meetings, etc</li>
-                        <ul>
-                            <li>Ask other precinct chairs to do the same and to post updates regularly</li>
-                            <li>Pass these group pages on to new precinct chairs as needed</li>
-                        </ul>
-                        <li>Overhaul the county GOP website and all related sites</li>
-                        <li>Start building a web platform to facilitate communication between local leaders and constituents for city and county matters</li>
-                        <li>Build a service to connect those who want to get involved with local politics with candidates and could use that help</li>
-                        <li>Build a service to find and help conservative candidates run for office to contest Democratic offices</li>
+                        <li>Ask other precinct chairs to do the same and to post updates regularly</li>
+                        <li>Pass these group pages on to new precinct chairs as needed</li>
                     </ul>
+                    <li>Overhaul the county GOP website and all related sites</li>
+                    <li>Start building a web platform to facilitate communication between local leaders and constituents for city and county matters</li>
+                    <li>Build a service to connect those who want to get involved with local politics with candidates and could use that help</li>
+                    <li>Build a service to find and help conservative candidates run for office to contest Democratic offices</li>
+                </ul>
 
-                    <p>These changes are intended to have the cumulative effect of unifying the GOP and developing leadership that can prevent the state from turning blue in the next decade.</p>
+                <p>These changes are intended to have the cumulative effect of unifying the GOP and developing leadership that can prevent the state from turning blue in the next decade.</p>
 
-                </div>
+            </div>
 
-            </div><!-- .row -->
+            <div class="col-md-5 col-sm-12 hidden-xs wow bounceInLeft">
+                <img class="avatar" src="/images/convention.jpg" alt="">
+            </div>
 
-        </div><!-- .container -->
+        </div><!-- .row -->
+
+    </div><!-- .container -->
 
     </section>
 
