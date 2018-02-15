c09ecd2d925229afa86a95df591c7748f45bdb92
Joshua Foxworth <jfoxworth@cadwolf.com>
2018-02-14 21:36:19 -0600

    image add

--- resources/views/candidate.blade.php
+++ resources/views/candidate.blade.php
@@ -11,7 +11,7 @@
                         <p>Candidate for Precinct Chair - 0487</p>
                     </div>
 
-                    <div class="col-md-9 col-sm-6 wow">
+                    <div class="col-md-7 col-sm-6 wow">
                         <h3>A bit about me</h3>
                         <p>I am an engineer, entrepreneur, and web developer that has lived here in Brittany Lakes since 2008. I am married with two young boys and I am a member of Bay Area Church. My wife and I are both aerospace engineers and graduates of UT Austin. Most evenings, you can find us out by Willow Pond with the kids.</p>
 
@@ -24,15 +24,19 @@
                         </ul>
                     </div>
 
-                    <div class="col-md-3 col-sm-6 hidden-xs wow bounceInLeft">
+                    <div class="col-md-5 col-sm-6 hidden-xs wow bounceInLeft">
                         <img class="avatar" src="/images/drawingroom.jpg" alt="">
                     </div>
 
                 </div>
 
+            </div>
+
+            <div class="container">
+
                 <div class="row">
 
-                    <div class="col-md-12 col-sm-12 wow">
+                    <div class="col-md-7 col-sm-12 wow">
                         <h3>What I want to do as precinct chair</h3>
                         
                         <p>The GOP is facing a number of problems within the county, the congressional district, and the state. These problems include getting information to our constituents, getting feedback on local and national issues, and an opponent with a web presence that is much more organized. I want to change all of that, so here is what I would do as precinct chair ...</p>
@@ -51,6 +55,10 @@
 
                         <p>These changes are intended to have the cumulative effect of unifying the GOP and developing leadership that can prevent the state from turning blue in the next decade.</p>
 
+                    </div>
+
+                    <div class="col-md-5 col-sm-12 hidden-xs wow bounceInLeft">
+                        <img class="avatar" src="/images/convention.jpg" alt="">
                     </div>
 
                 </div><!-- .row -->
